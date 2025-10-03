bec04c0b4efd8bc692c0e7b8d493618b87814d17
RainbowZephyr <10798017+RainbowZephyr@users.noreply.github.com>
2025-10-04 01:51:51 +0300

    feat: initialzing repo

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    kotlin("jvm") version "2.2.10"
+    id("com.gradleup.shadow") version "8.3.6"
+}
+
+group = "io.github.rainbowzephyr"
+var baseVersion = "0.1.0"
+version = System.getenv().getOrDefault("VERSION", "$baseVersion-SNAPSHOT")
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion("21")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
